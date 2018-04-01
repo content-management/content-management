6dc63418181835220ed5b942da3d06e5f4b46ded
Tiffany Schureman <tschureman@gmail.com>
2018-04-01 15:51:57 -0500

    editor: basic installed

---
+++ content-management-group-project/src/components/Editor/TextEditor.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+
+
+  class TextEditor extends React.Component {
+    handleEditorChange = (e) => {
+      console.log('Content was updated:', e.target.getContent());
+    }
+  
+    render() {
+      return (
+        <Editor
+          initialValue="<p>This is the initial content of the editor</p>"
+          init={{
+            plugins: 'link image code',
+            toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+          }}
+          onChange={this.handleEditorChange}
+        />
+      );
+    }
+  }
+  
+  export default Editor;
