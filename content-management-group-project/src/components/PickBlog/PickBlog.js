d4bd5c29abbce960a379fbfc11e53a7735962936
Xavier Carpenter <xavier.carpenter16@gmail.com>
2018-04-02 16:34:37 -0500

    add blog btn added

--- content-management-group-project/src/components/PickBlog/PickBlog.js
+++ content-management-group-project/src/components/PickBlog/PickBlog.js
@@ -9,42 +9,58 @@ class PickBlog extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        blogs: ""
+      blogs: "",
+      addBlog: false
+    
     };
+    this.addBlogClicked = this.addBlogClicked.bind(this);
   }
 
   componentDidMount() {
     this.props.getUser().then(() => {
-      
-      
-        axios.get(`/api/blogs/${this.props.user.id}`).then(response => {
-        this.setState({ blogs: response.data });
-      })
-      .catch(console.log());
+      axios
+        .get(`/api/blogs/${this.props.user.id}`)
+        .then(response => {
+          this.setState({ blogs: response.data });
+        })
+        .catch(console.log());
     });
   }
+
+  addBlogClicked() {
+    this.setState({ addBlog: true });
+  }
   render() {
-    let blogs = this.state.blogs && this.state.blogs.map((obj, i) => {
-      return (
-        <div key={i} >
-        <ul>
-          <Link to={`/Home/${obj.blog_id}`}>
-        <li> {obj.blog_name}</li>
-        </Link>
-        </ul>
-        </div> 
-      )
-    })
+    let blogs =
+      this.state.blogs &&
+      this.state.blogs.map((obj, i) => {
+        return <div key={i}>
+            <ul>
+              <Link to={`/Home/${obj.blog_id}`}>
+                <li >
+            
+                  {obj.blog_name}
+                </li>
+              </Link>
+            </ul>
+          </div>;
+      });
     console.log("user", this.props.user);
-    console.log(this.state.blogs);
+    console.log(this.state.currBlog);
     return (
       <div className="App">
         <div>
           <h1>Hello, {this.props.user.name}</h1>
-         
         </div>
         <h1>Which blog are you working on?</h1>
         {blogs}
+        <button onClick={this.addBlogClicked}>Add Blog</button>
+        
+        {this.state.addBlog === true ? <div>
+          <input type="text" placeholder="Your blog name"/>
+          <input type="submit"/>
+        </div> : null}
+     
       </div>
     );
   }
