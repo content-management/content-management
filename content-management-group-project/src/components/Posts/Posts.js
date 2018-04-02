7b9693e073265e9ca77a910793f880439dd9f520
Logan Simonsen <32521559+ENDgameERS@users.noreply.github.com>
2018-04-02 15:10:04 -0500

    stuff

--- content-management-group-project/src/components/Posts/Posts.js
+++ content-management-group-project/src/components/Posts/Posts.js
@@ -2,8 +2,24 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getUser } from "../../ducks/reducer";
 import { Link, withRouter } from "react-router-dom";
+import axios from "axios";
 
 class Posts extends Component {
+  constructor() {
+    super();
+  }
+  componentDidMount() {
+    this.props.getUser().then(
+      axios
+        .get(`/api/blogs/${this.props.user.id}`)
+        .then(response => {
+          console.log(response);
+          // this.setState({ blogs: response.data });
+        })
+        .catch(console.log())
+    );
+  }
+
   render() {
     return <div>See all your posts here </div>;
   }
