508f9fe556fa115130ad5c607fb01b1fb07a1c86
Logan Simonsen <32521559+ENDgameERS@users.noreply.github.com>
2018-04-03 10:55:41 -0500

    messing around with editor stuff

--- content-management-group-project/src/components/Editor/TextEditor.js
+++ content-management-group-project/src/components/Editor/TextEditor.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import { Editor } from '@tinymce/tinymce-react';
+import React from "react";
+import { Editor, textarea } from "@tinymce/tinymce-react";
 
+class TextEditor extends React.Component {
+  constructor() {
+    super();
+  }
+  componentDidMount() {}
 
-  class TextEditor extends React.Component {
-    
-  
-    render() {
+  render() {
+    return <textarea />;
+  }
+}
 
-      return (
-        <div>
-        <div><input type ='text' placeholder='Add Title Here'/></div>
-          <textarea></textarea>
-          </div>
-        );
-      }}
-  
-  export default TextEditor;
+export default TextEditor;
