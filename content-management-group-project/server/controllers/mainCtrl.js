ba4690c2de78f9bef0a3dfa4a6d9a49cf0d3dc13
Xavier Carpenter <xavier.carpenter16@gmail.com>
2018-04-04 15:14:24 -0500

    added delete post button and order post by recent first

--- content-management-group-project/server/controllers/mainCtrl.js
+++ content-management-group-project/server/controllers/mainCtrl.js
@@ -60,7 +60,6 @@ module.exports = {
   createPost: (req, res, next) => {
     const dbInstance = req.app.get("db");
     const { params, body } = req;
-  
 
     dbInstance
       .create_post([params.id, body.title, body.content])
@@ -72,12 +71,16 @@ module.exports = {
     const { params, body } = req;
 
     dbInstance
-      .update_post([
-        params.id,
-        body.title,
-        body.content
-    
-      ])
+      .update_post([params.id, body.title, body.content])
+      .then(() => res.status(200).json())
+      .catch(() => res.status(500).json());
+  },
+  deletePost: (req, res, next) => {
+    const dbInstance = req.app.get("db");
+    const { params } = req;
+
+    dbInstance
+      .delete_post([params.id])
       .then(() => res.status(200).json())
       .catch(() => res.status(500).json());
   }
