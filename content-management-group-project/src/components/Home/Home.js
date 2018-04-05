5defac66ce891d300ad9432c3fcb126b8b0d6940
Steve Smith <stevesmithsays@Steves-MacBook-Pro.local>
2018-04-05 14:34:08 -0500

    getting updated with current master

--- content-management-group-project/src/components/Home/Home.js
+++ content-management-group-project/src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux"; //connect to redux
 import { getUser } from "../../ducks/reducer"; //get user from redux
 import { Link, withRouter } from "react-router-dom";
-
+import axios from "axios";
 import Posts from "../Posts/Posts";
 import "../../styles/css/Home.css";
 
@@ -10,47 +10,72 @@ let temp = "";
 class Home extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      posts: ""
+    };
   }
   componentDidMount() {
-    this.props.getUser()
+    this.props.getUser().then(
+      axios
+        .get(`/api/posts/${this.props.match.params.id2}`)
+        .then(response => {
+          console.log("cool response", response.data);
+          this.setState({ posts: response.data });
+        })
+        .catch(console.log())
+    );
   }
   render() {
-    console.log("user", this.props);
-    return( 
-      <div>
-        <div className="home-body">
+    let myPost = this.state.posts &&
+      this.state.posts.map((obj, i) => {
+        console.log(obj.title);
+        return
+             ( <div key={i}>
 
+                <p>{obj.title}</p>
+
+              </div>)
+      });
+    return( <div>
+        <div className="home-body">
           <div className="home-greeting">
-          {!this.props.isLoading && <h1>
-            Welcome back, {this.props.user.name} !
-            </h1>}
+            {!this.props.isLoading && <h1>
+                Welcome back, {this.props.user.name} !
+              </h1>}
+              <h2>{this.props.match.params.id}</h2>
             {this.props.isLoading && <h2>Loading...</h2>}
-            <Posts/>
           </div>
 
           <div className="scroll-container">
-              <div className="horizontal-scroll-wrapper rectangles">
-                <div><p>blog posts here</p></div>
-                <div><p>blog posts here</p></div>
-                <div><p>blog posts here</p></div>
-                <div><p>blog posts here</p></div>
-                <div><p>blog posts here</p></div> 
+            <div className="horizontal-scroll-wrapper rectangles">
+              <div>
+                <p>blog posts here</p>
               </div>
+              <div>
+                <p>blog posts here</p>
+              </div>
+              <div>
+                <p>blog posts here</p>
+              </div>
+              <div>
+                <p>blog posts here</p>
+              </div>
+              <div>
+                <p>blog posts here</p>
+              </div>
+            </div>
           </div>
 
           <div className="cards-container">
-
-            <Link  to={`/Posts/${this.props.match.params.id}`}>
+            <Link to={`/Posts/${this.props.match.params.id}/${this.props.match.params.id2}`}>
               <div className="cards">Posts</div>
             </Link>
 
             <div className="cards">Pages</div>
             <div className="cards">Media</div>
-
           </div>
         </div>
-      </div>
-    )
+      </div>)
   }
 }
 
