06b9ca77db925a4f0183a51c2f71518a8aecc1d1
Xavier Carpenter <xavier.carpenter16@gmail.com>
2018-04-09 11:54:28 -0500

    added pages

---
+++ content-management-group-project/src/components/Editor/EditPage.js
@@ -0,0 +1,129 @@
+import React from "react";
+import { Editor, textarea } from "@tinymce/tinymce-react";
+import { connect } from "react-redux"; //connect to redux
+import { getUser} from "../../ducks/reducer"; //get user from redux
+import axios from "axios";
+import Header from "../Header/Header";
+import swal from "sweetalert";
+import "../../styles/css/EditPost.css";
+
+class EditPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      pages: "",
+      pageNaem: "",
+      content: ""
+    };
+    this.saveContent = this.saveContent.bind(this);
+  }
+  componentDidMount() {
+    console.log("window", window.tinyMCE);
+    axios
+         axios.get(`/api/pages/${this.props.user.id}`)
+        .then(response => {
+          this.setState({pages: response.data[0]})
+        this.setState({
+          pageName: this.state.pages.page_name,
+          content: this.state.pages.content
+        });
+      })
+      .catch(console.log());
+  }
+  handleEditorChange = e => {
+    // console.log(e.target.contentDocument);
+    this.setState({
+      content: e.target.getContent()
+    });
+  };
+  saveContent() {
+    console.log(this.state.content);
+    let body = {
+      pageName: this.state.pageName,
+      content: this.state.content
+    };
+    axios
+      .put(`/api/updatePage/${this.state.pages.page_id}`, body)
+      .then(results => {
+        swal("updated page");
+      })
+      .then(window.history.back());
+  }
+  render() {
+console.log(this.state.pages);
+console.log(this.state.pageName);
+console.log(this.state.content);
+
+    let pageName = this.state.pages.page_name;
+    // let content = this.state.post.content;
+    let content = this.state.pages.content;
+    return (
+      <div>
+        <Header />
+        <div className="editPostPage">
+          <input
+            type="text"
+            value={pageName}
+            onChange={e =>
+              this.setState({
+                pageName: e.target.value
+              })
+            }
+          />
+        </div>
+        <input
+          className="editTitle"
+          id="my-file"
+          type="file"
+          name="my-file"
+          style={{ display: "none" }}
+          onChange=""
+        />
+        {this.state.pages.content && (
+          <Editor
+            initialValue={content}
+            init={{
+              selector: "textarea",
+              height: 500,
+              plugins: [
+                "advlist autolink lists link image code charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table contextmenu paste imagetools wordcount",
+                "fullpage",
+                "save"
+              ],
+              toolbar:
+                "formatselect | insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | code | imageupload | fullpage ",
+              content_css: "//www.tinymce.com/css/codepen.min.css",
+              file_browser_callback_types: "image",
+              file_picker_callback: function(callback, value, meta) {
+                if (meta.filetype == "image") {
+                  var input = document.getElementById("my-file");
+                  input.click();
+                  input.onchange = function() {
+                    var file = input.files[0];
+                    var reader = new FileReader();
+                    reader.onload = function(e) {
+                      console.log("name", e.target.result);
+                      callback(e.target.result, { alt: file.name });
+                    };
+                    reader.readAsDataURL(file);
+                  };
+                }
+              },
+              paste_data_images: true
+            }}
+            onChange={this.handleEditorChange}
+          />
+        )}
+        <button onClick={this.saveContent}>Save</button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => state;
+
+export default connect(mapStateToProps, { getUser })(
+  EditPage
+);
