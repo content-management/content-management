082eaa3ccb7b92f455c5dc628253cc452cd3d111
Tiffany Schureman <tschureman@gmail.com>
2018-04-10 10:59:54 -0500

    deleted unused components, stying pick a blog page after changes to view

--- content-management-group-project/src/routes.js
+++ content-management-group-project/src/routes.js
@@ -4,8 +4,8 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Splash from "./components/Splash/Splash";
 import PickBlog from "./components/PickBlog/PickBlog";
-import PageView from "./components/Pages/PageView";
-import PostView from "./components/Posts/PostView";
+// import PageView from "./components/Pages/PageView";
+// import PostView from "./components/Posts/PostView";
 import NewPost from "./components/Editor/NewPost";
 import EditPost from "./components/Editor/EditPost";
 import EditPage from "./components/Editor/EditPage";
@@ -18,8 +18,8 @@ export default (
     <Route exact path="/pickblog/:id" component={PickBlog} />
     <Route exact path="/Home/:id/:id2" component={Home} />{" "}
     {/*id-blog name, id2-blogid  */}
-    <Route exact path="/PageView" component={PageView} />
-    <Route exact path="/PostView" component={PostView} />
+    {/* <Route exact path="/PageView" component={PageView} /> */}
+    {/* <Route exact path="/PostView" component={PostView} /> */}
     <Route exact path="/NewPost/:id" component={NewPost} />
     <Route exact path="/EditPost/:id" component={EditPost} />
     <Route exact path="/EditPage/:id" component={EditPage} />
