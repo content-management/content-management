e298d981265aa901ce2efff682ca7ac493440594
Tiffany Schureman <tschureman@gmail.com>
2018-04-12 16:24:09 -0500

    made html display as text

--- content-management-group-project/src/components/Credentials/Credentials.js
+++ content-management-group-project/src/components/Credentials/Credentials.js
@@ -53,9 +53,34 @@ class Credentials extends Component {
           <div className="box3">
             <img src={Instructions}/>
           </div>
-          <h2>Have your own template?</h2>
+          <h2>Have your own template? Copy this in your body tag.</h2>
           <div className="box4">
-            <img src={Instructions2}/>
+          <pre>{`<div class="demo" id="demo1"></div> <div class="demo" id="demo2"></div>
+    <div class="demo" id="demo3"></div>
+    <div class="demo" id="demo4"></div>
+    <div class="demo" id="demo5"></div>
+    <script>
+        
+        function loadDoc() {
+            let results = {};
+            let blogID ="" // <<=REPLACE # WITH BLOG ID HERE`}
+           { "const url = `http://localhost:3001/api/posts/${blogID}`; //" }
+           {`// EXAMPLE 'http://localhost:3001/api/posts/999'; WHERE 999 IS YOUR BLOG ID NUMBER`}
+            {`
+            axios.get(url).then(response => {
+           results = response.data;
+           document.getElementById("demo1").innerHTML = "<div style='text-align: left'>" + results[0].date+ "</div>" + results[0].content;
+           document.getElementById("demo2").innerHTML = "<div style='text-align: left'>" + results[1].date+ "</div>" + results[1].content;
+           document.getElementById("demo3").innerHTML = "<div style='text-align: left'>" + results[2].date+ "</div>" + results[2].content;
+           document.getElementById("demo4").innerHTML = "<div style='text-align: left'>" + results[3].date+ "</div>" + results[3].content;
+           document.getElementById("demo5").innerHTML = "<div style='text-align: left'>" + results[4].date+ "</div>" + results[4].content;
+        })
+        .catch(console.log())
+    }
+        
+    </script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>`}</pre>
+            {/* <img src={Instructions2}/> */}
           </div>
           <p className="more-instr">Detailed Instructions</p>
         </div>
