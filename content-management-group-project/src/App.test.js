663a5442f0c130bc96b527ced048ae1bccf50512
Steve Smith <stevesmithsays@Steves-MacBook-Pro.local>
2018-04-13 14:36:49 -0500

    header update merge

--- content-management-group-project/src/App.test.js
+++ content-management-group-project/src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from "react-redux";
+import store from "./store";
+import { HashRouter as Router } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
